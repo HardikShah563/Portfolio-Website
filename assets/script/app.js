357dc1795406cd058dc929017655ea1ea6cb52f8
HardikShah563 <hardikts@gmail.com>
2023-01-04 22:14:26 +0530

    basic structure built

---
+++ assets/script/app.js
@@ -0,0 +1,7 @@
+let box = document.getElementById("box");
+let toggleSwitch = document.getElementById('toggleSwitch');
+
+toggleSwitch.addEventListener('click', () => {
+    // document.box.classList.toggle('dark');
+    box.classList.toggle('light');
+});
